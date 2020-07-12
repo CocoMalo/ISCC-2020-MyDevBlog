34eca46015a795de5bab82676d10d59201d821fe
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-12 14:40:27 +0200

    1

---
+++ front-offiche.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <header>
+        </header>
+
+        <nav>
+        </nav>
+
+        <main>
+        </main>
+        
+        <footer>
+        </footer>
+    </body>
+</html>
