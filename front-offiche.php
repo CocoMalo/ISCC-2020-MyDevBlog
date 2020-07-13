c2b11bf4ac5cbba468245aa9fc89896fd8644f70
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-13 10:06:29 +0200

    2

--- front-offiche.php
+++ front-offiche.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title> Accueil </title>
     </head>
 
     <body>
         <header>
+            <h1> My Dev Blog </h1>
         </header>
 
         <nav>
+            <a href= "front-office.php" > Accueil </a>
+            <a href= ""> Articles </a>
         </nav>
 
         <main>
         </main>
-        
+
         <footer>
         </footer>
     </body>
