9131fb82939f03a67a3d390cb9c916499799778a
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-15 01:18:19 +0200

    9

---
+++ form-connexion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Connexion </title>
+    </head>
+
+    <body>
+        <form action="/action_page.php">
+        
+            <label for="iname"> Identifiant </label>
+            <input type="text" id="login" name="iname"></br>
+
+            <label for="mname"> Mot de passe </label>
+            <input type="text" id="password" name="mname"><br>
+
+            <input type="submit" value="Connexion">
+        </form>
+    </body>
+</html>
