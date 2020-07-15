0df5e017d9c53367828be13498e3274495bb252b
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-15 10:03:41 +0200

    11

--- Back/form-connexion.php
+++ Back/form-connexion.php
@@ -7,13 +7,14 @@
     <body>
         <form action="/action_page.php">
         
-            <label for="iname"> Identifiant </label>
-            <input type="text" id="login" name="iname"></br>
+            <label for="username"> Identifiant </label>
+            <input type="text" id="login" name="username"></br>
 
             <label for="mname"> Mot de passe </label>
             <input type="text" id="password" name="mname"><br>
 
             <input type="submit" value="Connexion">
+
         </form>
     </body>
 </html>
