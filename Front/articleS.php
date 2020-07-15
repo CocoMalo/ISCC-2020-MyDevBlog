e58847c4042b6d486ce4f77c813e10177351e604
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-15 19:31:31 +0200

    17

--- Front/articleS.php
+++ Front/articleS.php
@@ -10,6 +10,20 @@
     <?php
             include "front.php"
         ?>
+    <?php
+            $pdo = connect_to_database(MyDevBlog);
+
+            $query = $pdo->query ("SELECT * FROM articles"); 
+            $user = $query->fetch();
+            var_dump($user);
+        
+            $users = $pdo->query("SELECT * FROM articles")->fetchAl1();
+        
+            var_dump($users); 
+            foreach ($users as $user) { 
+                echo $user['articles']."<br/>"; 
+            }
+        ?>
     
     <main>
             <h2> Articles </h2>
